e88b518e5341818366a5be1b0e23e8ceee95518e
Amy Tang <amy@pathfactory.com>
2019-06-19 14:00:54 -0400

    rna_transcription refactored

--- elixir/rna-transcription/rna_transcription.exs
+++ elixir/rna-transcription/rna_transcription.exs
@@ -9,25 +9,15 @@ defmodule RNATranscription do
   """
   @spec to_rna([char]) :: [char]
   def to_rna(dna) do
-    # set dna from charlist (single quotes) to string (double quotes)
-    list = String.graphemes(to_string(dna))
-
-    # Map through dna and set corresponding dna to rna chars
-    rna_strand =
-      Enum.map(list, fn dna ->
-        case dna do
-          "G" -> "C"
-          "C" -> "G"
-          "T" -> "A"
-          "A" -> "U"
-          _ -> "Error"
-        end
-      end)
-
-    rna_strand
-    # set array back into a string
-    |> to_string
-    # set string back to charlist
-    |> to_charlist
+    # Map through dna
+    dna
+    |> Enum.map(
+      &%{
+        ?G => ?C,
+        ?C => ?G,
+        ?T => ?A,
+        ?A => ?U
+      }[&1]
+    )
   end
 end
